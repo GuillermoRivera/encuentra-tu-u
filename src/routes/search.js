9eeb546ff541a129f5d8b72768b148d5559d403c
Juan Guillermo Rivera Orozco <guillerivera32@gmail.com>
2017-12-06 00:37:38 -0500

    Render the results to the search page

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,10 +1,34 @@
 const express = require('express');
 
+const Institution = require('../models/institution');
+const Program = require('../models/program');
+
 const router = express.Router();
 
 /* GET search listing. */
 router.get('/', (req, res) => {
-  res.render('search', { title: 'Search page' });
+  if (req.query.type === 'institution') {
+
+    Program.getProgramsByInstitution(req.query.name, (error, data) => {
+      if (error) throw error;
+      if (data && data.length) {
+        res.render('search', { title: `Programas de educación superior en ${data[0].nombre_inst}`, data, tipo: req.query.type });
+      } else {
+        res.render('error', { error });
+      }
+    });
+  } else if (req.query.type === 'program') {
+    Institution.getInstitutionsByProgram(req.query.name, (error, data) => {
+      if (error) throw error;
+      if (data && data.length) {
+        res.render('search', { title: `${data[0].nombre_prog} en instituciones de Medellín`, data, tipo: req.query.type });
+      } else {
+        res.render('error', { error });
+      }
+    });
+  } else {
+    return new Error('Lo sentimos, criterio de búsqueda inválido');
+  }
 });
 
 module.exports = router;
