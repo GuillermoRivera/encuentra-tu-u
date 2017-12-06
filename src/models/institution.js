1becccf49cf112b2f2d1592ae6adb3bfb597ea31
Juan Guillermo Rivera Orozco <guillerivera32@gmail.com>
2017-12-06 00:35:51 -0500

    Create queries to get the information from database

--- src/models/institution.js
+++ src/models/institution.js
@@ -14,4 +14,24 @@ institutionModel.getInstitutions = (callback) => {
   });
 };
 
+institutionModel.getInstitutionsByProgram = (program, callback) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    const query = `SELECT * 
+    FROM instituciones i
+    INNER JOIN facultad_institucion fi
+    ON i.id_inst = fi.instituciones_id_inst
+    INNER JOIN facultad_institucion_programa fip
+    ON fi.instituciones_id_inst = fip.id_inst_pk AND fi.facultades_id_fac = fip.id_fac_pk
+    INNER JOIN programas p
+    ON p.id_prog = fip.id_prog_pk
+    WHERE p.nombre_prog LIKE '${program}%';`;
+
+    connection.query(query, (error, rows) => {
+      callback(null, rows);
+      connection.release();
+      if (error) throw error;
+    });
+  });
+};
 module.exports = institutionModel;
--- src/models/program.js
+++ src/models/program.js
@@ -14,4 +14,25 @@ programModel.getPrograms = (callback) => {
   });
 };
 
+programModel.getProgramsByInstitution = (institution, callback) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    const query = `SELECT * 
+    FROM instituciones i
+    INNER JOIN facultad_institucion fi
+    ON i.id_inst = fi.instituciones_id_inst
+    INNER JOIN facultad_institucion_programa fip
+    ON fi.instituciones_id_inst = fip.id_inst_pk AND fi.facultades_id_fac = fip.id_fac_pk
+    INNER JOIN programas p
+    ON p.id_prog = fip.id_prog_pk
+    WHERE i.nombre_inst LIKE '${institution}%';`;
+
+    connection.query(query, (error, rows) => {
+      callback(null, rows);
+      connection.release();
+      if (error) throw error;
+    });
+  });
+};
+
 module.exports = programModel;
